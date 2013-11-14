04a2f8874d239c6516179c2e1770407b874790f6
Marcio Castro <mbcastro@gmail.com>
2013-11-14 21:52:58 +0100

    Update!

--- noc-latency/run.sh
+++ noc-latency/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-k1-jtag-runner --multibinary=noc-cost.mpk --exec-multibin=IODDR0:master 01 > tmp.txt
-cut -d " " -f3 tmp.txt > data.csv
+k1-jtag-runner --multibinary=bin/noc-latency.mpk --exec-multibin=IODDR0:master 01 > tmp.txt
+cut -d " " -f3 tmp.txt > output/data.csv
 rm tmp.txt
