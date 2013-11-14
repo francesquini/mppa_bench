8b43e4cca2651a853972c6cbd68b96ea1b907387
Marcio Castro <mbcastro@gmail.com>
2013-11-14 22:06:37 +0100

    Updating plot.R

--- noc-latency/output/plot.R
+++ noc-latency/output/plot.R
@@ -8,28 +8,35 @@ read_latency <- function (filename) {
     return (x)
 }
 
-plot_latency <- function (f, er, scale) {
-    x <- read_latency (er)
-    ggplot (x, aes (factor(size), time, group = direction, colour = factor(direction, labels=c("master-slave","slave-master")))) +
+plot_latency <- function (f, er, scale) {   
+   x <- read_latency (er)
+ 
+   plot <- ggplot (x, aes (factor(size), time, group = direction, colour = factor(direction, labels=c("master-slave","slave-master")))) +
     theme_bw () +
     geom_line() +
     xlab ("Number of bytes") +
     ylab ("Time (in microseconds)") +
     guides(colour = guide_legend (nrow = 1)) +
-    scale_y_continuous(breaks = c(scale)) +
     theme (legend.title = element_blank (),
            legend.position = "bottom",
            legend.direction = "vertical",
            axis.text.x = element_text(angle = 45, hjust = 1)) +
-    ggsave (f)
+   
+   if ( !is.null(scale) )
+      plot <- plot + scale_y_continuous(breaks = c(scale))
+ 
+ plot
+ 
+ ggsave (f)
 }
 
 #------------------------------------------------------------------------------
 
-plot_latency ("channel-1-1.pdf", "channel-1-1.csv", seq(0, 14000, 1000))
+#plot_latency ("channel-1-1.pdf", "channel-1-1.csv", seq(0, 14000, 1000))
 #plot_latency ("channel-1-2.pdf", "channel-1-2.csv", seq(0, 28000, 2000))
 #plot_latency ("channel-1-4.pdf", "channel-1-4.csv", seq(0, 56000, 4000))
 #plot_latency ("channel-1-8.pdf", "channel-1-8.csv", seq(0, 112000, 8000))
 #plot_latency ("channel-1-16.pdf", "channel-1-16.csv", seq(0, 224000, 16000))
+#plot_latency ("portal-1-1.pdf", "portal-1-1.csv", seq(0, 100000, 2000))
 
-plot_latency ("portal-1-1.pdf", "portal-1-1.csv", seq(0, 100000, 2000))
+plot_latency ("plot.pdf", "data.csv")
