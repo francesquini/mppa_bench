726d14976be70894c740a1bd4c18c28ec8304920
Marcio Castro <mbcastro@gmail.com>
2013-11-29 02:27:35 +0100

    Stream now runs also on IO-cluster.

--- stream/run_mppa.sh
+++ stream/run_mppa.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-k1-jtag-runner --multibinary=stream.mpk --exec-multibin=IODDR0:master
+printf "Running stream on cluster\n"
+k1-jtag-runner --multibinary=stream_master_slave.mpk --exec-multibin=IODDR0:master
+
+printf "Running stream on IO-node\n"
+k1-jtag-runner --multibinary=stream_master.mpk --exec-multibin=IODDR0:stream
